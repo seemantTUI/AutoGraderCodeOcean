00b1876fd367da10031b9e6de742102b27fdcfc7
seemantTUI <149387514+seemantTUI@users.noreply.github.com>
2024-02-25 12:35:05 +0100

    VWDS7
    Integration of generative language AIs for feedback generation in the autograder CodeOcean

---
+++ chat_gpt_request.rb
@@ -0,0 +1,26 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+class ChatGptRequest
+  def request_gpt(prompt)
+    url = URI.parse('https://api.openai.com/v1/chat/completions')
+    data = {
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'assistant', content: prompt }]
+    }
+    api_key = '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'
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    request = Net::HTTP::Post.new(url.path, {'Content-Type' => 'application/json',
+                                             'Authorization' => "Bearer #{api_key}"})
+    request.body = data.to_json
+
+    response = http.request(request)
+    json_response = JSON.parse(response.body)
+    content = json_response['choices'][0]['message']['content']
+
+    return content
+  end
+end
+
---
+++ py_unit_adapter.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+require 'chat_gpt_request'
+
+class PyUnitAdapter < TestingFrameworkAdapter
+  COUNT_REGEXP = /Ran (\d+) test/
+  FAILURES_REGEXP = /FAILED \(.*failures=(\d+).*\)/
+  ERRORS_REGEXP = /FAILED \(.*errors=(\d+).*\)/
+  ASSERTION_ERROR_REGEXP = /^(ERROR|FAIL):\ (.*?)\ .*?^[^.\n]*?(Error|Exception):\s((\s|\S)*?)(>>>[^>]*?)*\s\s(-|=){70}/m
+
+  def self.framework_name
+    'PyUnit'
+  end
+
+  def parse_output(output)
+    # PyUnit is expected to print test results on Stderr!
+    count = output[:stderr].scan(COUNT_REGEXP).try(:last).try(:first).try(:to_i) || 0
+    failed = output[:stderr].scan(FAILURES_REGEXP).try(:last).try(:first).try(:to_i) || 0
+    errors = output[:stderr].scan(ERRORS_REGEXP).try(:last).try(:first).try(:to_i) || 0
+    begin
+      assertion_error_matches = Timeout.timeout(10.seconds) do
+        output[:stderr].scan(ASSERTION_ERROR_REGEXP).map do |match|
+          testname = match[1]
+          error = match[3].strip
+
+          if testname == 'test_assess'
+            error
+          else
+            "#{testname}: #{error}"
+          end
+        end || []
+      end
+    rescue Timeout::Error
+      Sentry.capture_message({stderr: output[:stderr], regex: ASSERTION_ERROR_REGEXP}.to_json)
+      assertion_error_matches = []
+    end
+    #chat_gpt_request = ChatGptRequest.new
+    #prompt = output[:stderr]
+    #response = chat_gpt_request.request_gpt(prompt)
+    #line_length = 50
+    #para = response.scan(/.{1,#{line_length}}/).join("\n")
+    #output[:stderr] = para
+    {count:, failed: failed + errors, error_messages: assertion_error_matches.flatten.compact_blank}
+  end
+end
